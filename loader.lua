3f98cbb5edae5f6e21343a829154d3147fca0377
rizzy <666xxrizzyxx666@gmail.com>
2025-11-26 20:34:13 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,5 @@
 -- loader.lua
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 local MainOptions = loadstring(game:HttpGet("https://raw.githubusercontent.com/666xxrizzyxx666-ship-it/XenonHub/main/main.lua"))()
 local ViewOptions = loadstring(game:HttpGet("https://raw.githubusercontent.com/666xxrizzyxx666-ship-it/XenonHub/main/view.lua"))()
 local UtilityOptions = loadstring(game:HttpGet("https://raw.githubusercontent.com/666xxrizzyxx666-ship-it/XenonHub/main/utility.lua"))()
