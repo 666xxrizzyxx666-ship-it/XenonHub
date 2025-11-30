ddfed0fc941bdf05d8d5f0ca285e5acf2f27831e
rizzy <666xxrizzyxx666@gmail.com>
2025-11-30 19:12:21 +0100

    Update utility.lua

--- utility.lua
+++ utility.lua
@@ -1,21 +1,3 @@
 local UtilityOptions = {}
 
-function UtilityOptions.InfiniteJump()
-    local Player = game:GetService("Players").LocalPlayer
-    local UIS = game:GetService("UserInputService")
-    UIS.JumpRequest:Connect(function()
-        Player.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
-    end)
-end
-
-function UtilityOptions.TeleportTool()
-    local Player = game:GetService("Players").LocalPlayer
-    local Mouse = Player:GetMouse()
-    Mouse.Button1Down:Connect(function()
-        if Mouse.Target then
-            Player.Character:MoveTo(Mouse.Hit.p)
-        end
-    end)
-end
-
-return UtilityOptions
+function UtilityOptions.InfiniteJump(state
